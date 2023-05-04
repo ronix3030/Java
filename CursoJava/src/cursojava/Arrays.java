373af12bc00784bfd0cba03f8ba5acbcb462cccb
ronix3030 <ronald3019@hotmail.com>
2023-05-03 22:28:12 -0500

    Noveno Viaje

--- CursoJava/src/cursojava/Arrays.java
+++ CursoJava/src/cursojava/Arrays.java
@@ -4,6 +4,8 @@
  */
 package cursojava;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author ronald
@@ -56,10 +58,12 @@ public class Arrays {
            }
           
           System.out.println("----------Matriz Multidimensional------");
-          
+  
           // Arraya con 2 dimensiones
-          
-          int[][] x =new int[2][3];
+          // Sirve para almacenar datos sea enteros o Letras 
+          // Siempre la primera es la cantidad de Filas y la 2da es la columna
+          // A la hora de interar es mas eficiente pero la flexibidad es menor
+          int[][] x =new int[2][3]; // x =indice
           x[0][0] =1; //Fila 1   // Columna 1
           x[0][1] =2; //Fila 1   // Columna 2
           x[0][2] =3; //Fila 1   // Columna 3
@@ -76,10 +80,88 @@ public class Arrays {
               }
           }
           
-          // Arraya con 3 dimensiones
+        System.out.println("3 FILAS ");
           
           
-}  
+          
+          int [][] y=new int [3][3];
+           
+          y[0][0]= 1; 
+          y[0][1]= 2; 
+          y[0][2]= 3; 
+          
+          y[1][0]= 4; 
+          y[1][1]= 5; 
+          y[1][2]= 6;
+          
+          y[2][0]= 7;
+          y[2][1]= 8; 
+          y[2][2]= 9;
+          
+          for (int[] array: y){
+              for (int datos: array){
+                  System.out.println(datos);
+              }
+          }
+          
+           // Arraya con 3 dimensiones
+                      System.out.println("MULTIDIMIMENSIONAL");
+          int [][][] p={
+        {
+            {1,2,3},
+            {4,5,6},
+            },
+              {
+                  {-1,2-3},
+                  {-4,-5,-6},
+        },
+        
+    };
+          
+          
+          System.out.println("------------EJEMPLO DE ARRAYLIST----------");
+          // Es una clase que permite almacenar elementos en una matriz dinamica
+          // Hay que importar la libreria de ArrayList
+          // Los arralist normalmente se usan en Proyectos
+          // Los arraylist es solo una linea y dinamicas
+          // Rendimiento menor pero mas flexible al programar 
+          ArrayList <Integer> list =new ArrayList<>();
+          list.add(10);
+          list.add(20);
+          System.out.println(list);
+          
+          ArrayList<Integer> list2=new ArrayList<>();
+          
+          list.add(1);
+          System.out.println("Otro ejemplo");
+          System.out.println("--Pero en String--");
+          //Declaracion
+          ArrayList<String> cadenitas;
+          // Crear Instancia
+          cadenitas= new ArrayList<>();
+          //Agregar Elementos
+          
+          cadenitas.add("Hola");
+          cadenitas.add("Como ");
+          cadenitas.add("Estas? ");
+          
+          //Recorrer Arraylist con for
+          
+          for(String datox:cadenitas){
+               System.out.println(datox);
+           }
+          
+          //Eliminar Elementos
+          cadenitas.remove(2); //Elimine la 2da linea 
+          for(String datox:cadenitas){
+               System.out.println(datox);
+           }
+          
+          
+          
+          
+          
+}
 } 
   
 
---
+++ CursoJava/src/cursojava/PracticasBucle.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cursojava;
+
+/**
+ *
+ * @author ronald
+ */
+public class PracticasBucle {
+       public static void main(String[] args){
+           
+           
+           
+           
+           
+       }
+    
+}
