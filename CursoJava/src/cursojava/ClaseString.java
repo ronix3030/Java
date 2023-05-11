41f5fd661110ab90cce645529b012576af80a7ca
ronix3030 <ronald3019@hotmail.com>
2023-05-10 21:09:48 -0500

    19 Viaje

---
+++ CursoJava/src/cursojava/ClaseString.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cursojava;
+
+/**
+ *
+ * @author ronald
+ */
+public class ClaseString {
+       public static void main(String[] args) {
+           System.out.println("----------Clase String---------- ");
+           System.out.println("Metodo charAt");
+           // Puedes ingresar a cada indice de la cadena o llamado tmb caracter
+           // Comienza de 0 a Infinito
+           String nombre="Hola";
+           System.out.println(nombre.charAt(3));  // Muestranos el Indice 3
+           System.out.println("Metodo length");
+           System.out.println(nombre.length());
+           
+           
+           
+           
+       }
+    
+}
